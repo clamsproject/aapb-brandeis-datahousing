d0dce261892b90b589b02df5239431964c05f022
leorabaumgarten <baumgartenleora@gmail.com>
2023-06-20 14:16:59 -0400

    added "obtaining" functionality

--- app.py
+++ app.py
@@ -30,21 +30,21 @@ def directory_search(guid):
 def file_typer(path):
     """determines the file type based on its extension"""
     file_types = {'.vtt': 'text', '.txt': 'text', '.mp3': 'audio', '.mp4': 'video', '.mov': 'video', '.xml': 'markup'}
-    if str(path.suffix) in file_types:
-        return file_types[str(path.suffix)]
+    if path.suffix in file_types:
+        return file_types[path.suffix]
     else:
         return 'other'
     
 def database_search(connection, guid, types):
     """searches the database for files"""
     if len(types) == 1:
-        paths = connection.execute("""SELECT file_type, server_path FROM map WHERE GUID=? and file_type=?;""", (guid, types[0])).fetchall()
+        paths = connection.execute("""SELECT file_type, server_path FROM map WHERE GUID=? and file_type=? GROUP BY file_type, server_path;""", (guid, types[0])).fetchall()
     elif len(types) == 2:
-        paths = connection.execute("""SELECT file_type, server_path FROM map WHERE GUID=? and file_type in (?, ?);""", (guid, types[0], types[1])).fetchall()
+        paths = connection.execute("""SELECT file_type, server_path FROM map WHERE GUID=? and file_type in (?, ?) GROUP BY file_type, server_path;""", (guid, types[0], types[1])).fetchall()
     elif len(types) == 3:
-        paths = connection.execute("""SELECT file_type, server_path FROM map WHERE GUID=? and file_type in (?, ?, ?);""", (guid, types[0], types[1], types[2])).fetchall()
+        paths = connection.execute("""SELECT file_type, server_path FROM map WHERE GUID=? and file_type in (?, ?, ?) GROUP BY file_type, server_path;""", (guid, types[0], types[1], types[2])).fetchall()
     else:
-        paths = connection.execute("""SELECT file_type, server_path FROM map WHERE GUID=?;""", (guid,)).fetchall()
+        paths = connection.execute("""SELECT file_type, server_path FROM map WHERE GUID=? GROUP BY file_type, server_path;""", (guid,)).fetchall()
     connection.commit()
     return paths
 
@@ -54,12 +54,25 @@ def insert_into_db(connection, guid, result):
     connection.execute("""INSERT INTO map VALUES (?, ?, ?);""", (guid, type, str(result)))
     connection.commit()
 
+def aapb_generate(guid, extension):
+    """generates a file from AAPB given a guid and file type"""
+    # NOTE: needs to be updated with AAPB API
+    root = Path(SEARCH_DIRECTORY)
+    dir = root.joinpath(RESULT_DIRECTORY)
+    if not dir.is_dir():
+        dir.mkdir()
+    filename = dir.joinpath(guid + extension)
+    filename.touch()
+    return filename
+    
+    
+
 @app.route('/')
 def main():
     return render_template('home.html')
 
-@app.route('/mapper', methods=['GET','POST'])
-def mapper():
+@app.route('/search', methods=['GET','POST'])
+def search():
     if request.method == 'POST':
         files = request.form.getlist('file_type')
         guid = request.form['GUID']
@@ -78,7 +91,19 @@ def mapper():
         connection.close()
         return render_template('search_results.html', GUID=guid, paths=paths)
     else:
-        return redirect(url_for('main'))
+        return render_template('search.html')
+    
+@app.route('/generate', methods=['GET','POST'])
+def add():
+    if request.method == 'POST':
+        extension = request.form['file_extension']
+        guid = request.form['GUID']
+        file = aapb_generate(guid, extension)
+        connection = get_db_connection()
+        insert_into_db(connection, guid, file)
+        return render_template('generate_results.html', GUID=guid, path=file)
+    else:
+        return render_template('generate.html')
     
 if __name__ == '__main__':
     initialize()
