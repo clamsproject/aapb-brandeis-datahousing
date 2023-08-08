4b6f8a872be20fc64b4ae57ac3bfcc780f6b0435
Keigh Rim <keigh.rim@gmail.com>
2023-08-08 19:02:50 -0400

    added fn to make guids short

--- app.py
+++ app.py
@@ -1,12 +1,21 @@
-from flask import Flask, render_template, request, redirect, url_for
-import sqlite3
 import argparse
+import sqlite3
+from collections import defaultdict
 from datetime import date
 from pathlib import Path
+
+from flask import Flask, render_template, request
 
 DATABASE = 'database.db'
 
 app = Flask(__name__)
+
+
+def shorten_guid(guid):
+    if guid.startswith('cpb'):
+        return '-'.join(guid[10:].split('.', 1)[0].split('-')[:2])
+    return guid
+
 
 def initialize(start):
     """initializes the database"""
@@ -17,6 +26,14 @@ def initialize(start):
     else:
         with open('schema.sql') as f:
             connection.executescript(f.read())
+    if start:
+        d = defaultdict(lambda: defaultdict(list))
+        for f in Path(SEARCH_DIRECTORY).glob("**/*"):
+            if f.name.startswith('cpb'):
+                d[shorten_guid(f.stem)][file_typer(f)].append(f)
+        # TODO (krim @ 8/8/23): bulk insert all findings (d) into the database 
+        # connection.execute`many`("""INSERT INTO map VALUES (?, ?, ?, ?, ?);""", (guid, type, str(result), date.today(), date.today()))
+
 
 def get_db_connection():
     """gets connection to the database in order to work with it"""
@@ -24,13 +41,15 @@ def get_db_connection():
     connection.row_factory = sqlite3.Row
     return connection
 
+
 def directory_search(guid):
     """returns the locations of all files in the SEARCH_DIRECTORY that begin with the given guid"""
     paths = []
     for file in Path(SEARCH_DIRECTORY).glob("**/*"):
-        if file.stem.startswith(guid):
+        if guid in file.stem:
             paths.append(file)
     return paths
+
 
 def file_typer(path):
     """determines the file type based on its extension"""
@@ -39,9 +58,11 @@ def file_typer(path):
         return file_types[path.suffix]
     else:
         return 'other'
-    
+
+
 def database_search(connection, guid, types):
     """searches the database for files"""
+    guid = shorten_guid(guid)
     if len(types) == 1:
         paths = connection.execute("""SELECT file_type, server_path FROM map WHERE GUID=? and file_type=? GROUP BY file_type, server_path;""", (guid, types[0])).fetchall()
         connection.execute("""UPDATE map SET date_last_accessed=? WHERE GUID=? and file_type=?;""", (date.today(), guid, types[0]))
@@ -57,11 +78,14 @@ def database_search(connection, guid, types):
     connection.commit()
     return paths
 
+
 def insert_into_db(connection, guid, result):
     """inserts new entry into the database"""
+    guid = shorten_guid(guid)
     type = file_typer(result)
     connection.execute("""INSERT INTO map VALUES (?, ?, ?, ?, ?);""", (guid, type, str(result), date.today(), date.today()))
     connection.commit()
+
 
 def aapb_generate(guid, extension):
     """generates a file from AAPB given a guid and file type"""
@@ -73,14 +97,14 @@ def aapb_generate(guid, extension):
     filename = dir.joinpath(guid + extension)
     filename.touch()
     return filename
-    
-    
+
 
 @app.route('/')
 def main():
     return render_template('home.html')
 
-@app.route('/search', methods=['GET','POST'])
+
+@app.route('/search', methods=['GET', 'POST'])
 def search():
     if request.method == 'POST':
         files = request.form.getlist('file_type')
@@ -101,7 +125,8 @@ def search():
         return render_template('search_results.html', GUID=guid, paths=paths)
     else:
         return render_template('search.html')
-    
+
+
 @app.route('/searchapi', methods=['GET'])
 def search_api():
     if 'file' in request.args:
@@ -123,8 +148,9 @@ def search_api():
         return paths[0]['server_path']
     else:
         return 'The requested file does not exist in our server'
-    
-@app.route('/generate', methods=['GET','POST'])
+
+
+@app.route('/generate', methods=['GET', 'POST'])
 def add():
     if request.method == 'POST':
         extension = request.form['file_extension']
@@ -135,11 +161,14 @@ def add():
         return render_template('generate_results.html', GUID=guid, path=file)
     else:
         return render_template('generate.html')
-    
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('-s', '--search_directory', nargs='?', help="the root directory that contains the files to search in")
-    parser.add_argument('-r', '--result_directory', nargs='?', help="the subdirectory you'd like to store new files in", default="newfiles")
+    parser.add_argument('-s', '--search_directory', nargs='?',
+                        help="the root directory that contains the files to search in")
+    parser.add_argument('-r', '--result_directory', nargs='?', help="the subdirectory you'd like to store new files in",
+                        default="newfiles")
     parser.add_argument('--host', nargs='?', help="the host name", default="0.0.0.0")
     parser.add_argument('-p', '--port', nargs='?', help="the port to run the app from", default="8001")
     parser.add_argument('-n', '--new_database', help="create the database from scratch", action='store_true')
