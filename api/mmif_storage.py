clamsproject/aapb-brandeis-datahousing

commit b1cdb4b8bacac4a26d66321bd7cc1cd712c47842

simplified pipeline_id generation code, as it's now implemented on sdk level

api/mmif_storage.py
@@ -8,6 +8,7 @@ from mmif import utils
 from clams_utils.aapb import guidhandler
 from flask import request, jsonify, Blueprint, current_app
 from mmif import Mmif
+from mmif.utils.cli.describe import generate_pipeline_identifier, split_appname_appversion
 
 from api import STORAGE_DIRECTORY
 
@@ -24,21 +25,6 @@ API_PREFIX = '/storeapi'
 
 class StorageServerError(Exception):
     pass
-
-
-def split_appname_appversion(long_app_id):
-    """
-    Helper method for splitting the app name and version number from a string. This
-    assumes the long identifier looks like "uri://APP_DOMAIN/APP_NAME/APP_VERSION"
-    """
-    app_path = Path(long_app_id).parts
-    app_name = app_path[2] if len(app_path) > 2 else None
-    app_version = app_path[3] if len(app_path) > 3 else None
-    if app_version is not None and app_name.endswith(app_version):
-        app_name = app_name[:-len(app_version) - 1]
-    if app_version == 'unresolvable':
-        app_version = None
-    return app_name, app_version
 
 
 def identifier_of_first_document(mmif_file: Mmif):
@@ -144,7 +130,7 @@ def download_mmif():
     data = json.loads(request.data.decode('utf-8'))
     # get both pipeline and guid from data
     # obtain pipeline using helper method
-    pipeline = pipeline_from_param_json(data)
+    pipeline = generate_pipeline_identifier(data)
     # get number of views for rewind if necessary
     num_views = len(data.get('pipeline', []))
     guid = data.get('guid')
@@ -170,9 +156,7 @@ def parse_parameters(view):
     """
     try:
         param_dict = view.metadata.parameters
-        # param_dict = {k: v.replace("'", '\"') for k, v in param_dict.items()}
-        # print(param_dict)
-        param_list = ['='.join(pair) for pair in param_dict.items()]
+        param_list = ['='.join([k, str(v)]) for k, v in param_dict.items()]
         param_list.sort()
         param_string = ','.join(param_list)
     except KeyError:
@@ -182,33 +166,6 @@ def parse_parameters(view):
     # NOTE: this is *not* for security purposes, so the usage of md5 is not an issue.
     param_hash = hashlib.md5(param_string.encode('utf-8')).hexdigest()
     return param_dict, param_hash
-
-
-# helper method for extracting pipeline
-def pipeline_from_param_json(param_json):
-    """
-    This method reads in a json containing the names of the pipelined apps and their
-    respective parameters, and then builds a path out of the pipelined apps and hashed
-    parameters.
-    """
-    pipeline = ""
-    for clams_app in param_json["pipeline"]:
-        # not using os path join until later for testing purposes
-        pipeline = pipeline + "/" + clams_app
-        # try to get param items
-        try:
-            param_list = ['='.join(pair) for pair in param_json["pipeline"][clams_app].items()]
-            param_list.sort()
-            param_string = ','.join(param_list)
-        # throws attribute error if empty (because empty means it's a set and not dict)
-        except AttributeError:
-            param_string = ""
-        # hash parameters
-        param_hash = hashlib.md5(param_string.encode('utf-8')).hexdigest()
-        pipeline = pipeline + "/" + param_hash
-    # removing first "/" so it doesn't mess with os.path.join later
-    pipeline = pipeline[1:]
-    return pipeline
 
 
 def zero_guid_download_response(pipeline: str):

commit 0de8ab1d337719187914129ec80a1af0168a9659

fixed download API broken in b1cdb4b by mistake

api/mmif_storage.py
@@ -1,21 +1,18 @@
-import hashlib
 import io
 import json
 import os
 import re
 import zipfile as zf
-from collections import Counter
 from pathlib import Path
-from typing import List, Dict, Tuple, Union
+from typing import Union
 from zipfile import ZIP_DEFLATED
 
 from clams_utils.aapb import guidhandler
 from flask import request, jsonify, Blueprint, current_app, send_file
 from mmif import Mmif
 from mmif import utils
-from mmif.utils.workflow_helper import _split_appname_appversion
 from mmif.utils.workflow_helper import generate_param_hash
-from mmif.utils.workflow_helper import group_views_by_app
+from mmif.utils.workflow_helper import generate_workflow_identifier
 
 from api import STORAGE_DIRECTORY
 
@@ -40,51 +37,6 @@ def identifier_of_first_document(mmif_file: Mmif):
     return None
 
 
-def generate_workflow_identifier(data: Mmif) -> Tuple[str, List[Dict]]:
-    """
-    Mostly copied from mmif.utils.workflow_helper.generate_workflow_identifier version 1.2.1
-    with addition of getting the raw parameter dicts.
-    TODO In the future when mmif.utils.workflow_helper.generate_workflow_identifier supports returning
-    parameter dicts, we can switch to that.
-    """
-    segments = []
-    # First prefix is source information, sorted by document type
-    sources = Counter(doc.at_type.shortname for doc in data.documents)
-    segments.append('-'.join([f'{k}-{sources[k]}' for k in sorted(sources.keys())]))
-
-    # Group views into runs
-    grouped_apps = group_views_by_app(data.views)
-
-    param_dicts = []
-    for app_execution in grouped_apps:
-        # Use the first view in the run as representative for metadata
-        first_view = app_execution[0]
-
-        # Skip runs where the representative view has errors or warnings
-        if first_view.has_error() or first_view.has_warnings():
-            continue
-
-        app = first_view.metadata.get("app")
-        if app is None:
-            continue
-        app_name, app_version = _split_appname_appversion(app)
-
-        # Use raw parameters from the first view for reproducibility
-        try:
-            param_dict = first_view.metadata.parameters
-        except (KeyError, AttributeError):
-            param_dict = {}
-        param_dicts.append(param_dict)
-
-        param_hash = generate_param_hash(param_dict)
-
-        # Build segment: app_name/version/hash
-        name_str = app_name if app_name else "unknown"
-        version_str = app_version if app_version else "unversioned"
-        segments.append(f"{name_str}/{version_str}/{param_hash}")
-
-    return '/'.join(segments), param_dicts
-
 
 @bp.post(f"{API_PREFIX}/upload")
 def upload_mmif():
@@ -100,14 +52,10 @@ def upload_mmif():
         guid = guidhandler.get_aapb_guid_from(mmif[doc_id].location)
         cur_root = Path(STORAGE_DIRECTORY)
 
-        wfid, param_dicts = generate_workflow_identifier(mmif)
-        # wf_id syntax is source_info/app1name/app1version/app1paramhash/app2name/...
-        # need to pull appname/appversion from the id and find corresponding views to extract params from view metadata
+        wfid, param_dicts = generate_workflow_identifier(
+            mmif, return_param_dicts=True)
+        # wf_id syntax is app1name/app1version/app1paramhash/app2name/...
         segments = wfid.split('/')
-        # start from the first "source info" segment
-        cur_root = cur_root / Path(segments[0])
-        segments = segments[1:]
-        # make sure segments is multiple of 3
         if len(segments) % 3 != 0:
             return upload_error_response(ValueError("Malformed workflow identifier"))
         mmif_fname = None
@@ -179,8 +127,34 @@ def upload_error_response(e):
 
 @bp.post(f"{API_PREFIX}/download")
 def download_mmif():
+    # TODO (krim @ 2025-12-17): need to update this after https://github.com/clamsproject/aapb-brandeis-datahousing/issues/34 is resolved
+    """
+    request payload format:
+
+    {
+        "workflow": { "swt-detection/v2.0-38-g7838415": {"pretty": "True"} },
+        "guid": "NON-EXISTING GUID"
+    }
+    or
+    {
+        "workflow": { "whisper-wrapper/v3": {"modelSize": "tiny"} },
+        "guid": ["cpb-aacip-507-154dn40c26", "cpb-aacip-507-v40js9j432", "NO-SUCH-GUID"]
+    }
+    {"workflow": {"swt-detection/v2.0-38-g7838415": {"pretty": "True"}}}
+
+    """
     data = json.loads(request.data.decode('utf-8'))
-    wfid = generate_workflow_identifier(data)
+    # build workflow storage path from request JSON
+    # e.g. {"workflow": {"swt-detection/v2.0": {"pretty": "True"}}}
+    # becomes "swt-detection/v2.0/paramhash"
+    wfid_segments = []
+    for clams_app, params in data["workflow"].items():
+        wfid_segments.append(clams_app)
+        try:
+            wfid_segments.append(generate_param_hash(params))
+        except AttributeError:
+            wfid_segments.append(generate_param_hash({}))
+    wfid = '/'.join(wfid_segments)
     # get number of views for rewind if necessary
     num_views = len(data.get('workflow', []))
     guid = data.get('guid')